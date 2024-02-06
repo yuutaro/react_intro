842b1a1477fa87aa4d70a92916c8fb71a663c05e
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-02-07 00:09:39 +0900

    もろもろ

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './css/dist.css';
 import reportWebVitals from './reportWebVitals';
 
-// components
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
